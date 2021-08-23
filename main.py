c99bb482a5e64e37cfade676bd88760ce00ce9b6
Xar0P <pietroricardocres@hotmail.com>
2021-08-23 18:59:22 -0300

    update

--- main.py
+++ main.py
@@ -3,16 +3,32 @@ from hidden import Hidden
 from user_information import User_information
 import DiscordUtils
 import discord
+import datetime
+import humanfriendly
 
 bot = Bot(Hidden().token())
 client = bot.connect()
 
 music = DiscordUtils.Music()
 
+MAIN_COLOR = 0x690FC3
+
+def embed_music(ctx,song,status):
+    return discord.Embed(title=f'{status} {song.title}',
+            url=song.url,
+            color=MAIN_COLOR,
+            timestamp=datetime.datetime.utcnow(),
+            description=f"\nDuração: {humanfriendly.format_timespan(song.duration).replace('minutes','minutos').replace('seconds','segundos')}\nCanal: [{song.channel}]({song.channel_url})"
+        ).set_image(url=song.thumbnail
+        ).set_footer(text=f"Loop: {'✅' if song.is_looping else '❌'}", icon_url=ctx.guild.icon_url if ctx.guild.icon is not None else 'https://cdn.discordapp.com/embed/avatars/1.png'
+        ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+
+
 @client.event
 async def on_ready():
-    print('Tamo on')
-    print(client.get_guild(869709006439075890).members)
+    print('Online')
+
+
 
 @client.command()
 async def avatar(ctx, user_reference=''):
@@ -34,9 +50,6 @@ async def avatar(ctx, user_reference=''):
             await ctx.send('Esse usuário não existe no servidor.')
 
 
-# @client.command()
-# async def join(ctx):
-#     await ctx.author.voice.channel.connect() #Joins author's voice channel
 
 @client.command()
 async def leave(ctx):
@@ -62,17 +75,14 @@ async def play(ctx, *, url):
         await player.queue(url, search=True)
 
         song = await player.play()
-        await ctx.send(f"Playing {song.name}")
+        await ctx.send(embed=embed_music(ctx,song,'Tocando'))
     else:
         song = await player.queue(url, search=True)
-        await ctx.send(embed=discord.Embed(
-            title=f"Adicionado {song.name}",
-            url = song.url,
-            color = 0x690FC3,
-            description = f'Duração: {song.duration}\nCanal: [{song.channel}]({song.channel_url})'
-            ).set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url
-            ).set_thumbnail(url=song.thumbnail
-            ).set_footer(text=f"Música adicionada para a lista | Loop: {'✅' if song.is_looping else '❌'}", icon_url=ctx.guild.icon.url if ctx.guild.icon is not None else "https://cdn.discordapp.com/embed/avatars/1.png"))
+        await ctx.send(embed=embed_music(ctx,song,'Adicionado'))
+
+
+    
+
 
 
 @client.command()
@@ -81,17 +91,23 @@ async def pause(ctx):
     song = await player.pause()
     await ctx.send(f"Paused {song.name}")
 
+
+
 @client.command()
 async def resume(ctx):
     player = music.get_player(guild_id=ctx.guild.id)
     song = await player.resume()
     await ctx.send(f"Resumed {song.name}")
 
+
+
 @client.command()
 async def stop(ctx):
     player = music.get_player(guild_id=ctx.guild.id)
     await player.stop()
     await ctx.send("Stopped")
+
+
 
 @client.command()
 async def loop(ctx):
@@ -102,16 +118,22 @@ async def loop(ctx):
     else:
         await ctx.send(f"Disabled loop for {song.name}")
 
+
+
 @client.command()
 async def queue(ctx):
     player = music.get_player(guild_id=ctx.guild.id)
     await ctx.send(f"{', '.join([song.name for song in player.current_queue()])}")
+
+
 
 @client.command(aliases=['np','tocando'])
 async def nowplaying(ctx):
     player = music.get_player(guild_id=ctx.guild.id)
     song = player.now_playing()
     await ctx.send(song.name)
+
+
 
 @client.command()
 async def skip(ctx):
@@ -122,11 +144,15 @@ async def skip(ctx):
     else:
         await ctx.send(f"Skipped {data[0].name}")
 
+
+
 @client.command()
 async def volume(ctx, vol):
     player = music.get_player(guild_id=ctx.guild.id)
     song, volume = await player.change_volume(float(vol) / 100) # volume should be a float between 0 to 1
     await ctx.send(f"Changed volume for {song.name} to {volume*100}%")
+
+
 
 @client.command()
 async def remove(ctx, index):
